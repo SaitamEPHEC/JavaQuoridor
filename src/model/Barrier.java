2849673008c4a6f77a7ccb4cc9f92ea1503a03e3
ATHOOS <adrien.chelle0712@hotmail.fr>
2018-12-21 14:46:59 +0100

    del com set&get

--- src/model/Barrier.java
+++ src/model/Barrier.java
@@ -46,66 +46,42 @@ public class Barrier {
 			posX2 = posX;
 		}
 	}
-	/**
-	 * Permet d'obtenir la premiere position de la barriere sur l'axe des ordonnees (Y)
-	 * @return la position Y1
-	 */
+	
 	public int getPosY1() {
 		return posY1;
 	}
 	
-	/**
-	 * Permet d'obtenir la premiere position de la barriere sur l'axe des abscisses (X)
-	 * @return la position X1
-	 */
+	
 	public int getPosX1() {
 		return posX1;
 	}
 
-	/**
-	 * Permet d'obtenir la seconde position de la barriere sur l'axe des ordonnees (Y)
-	 * @return la position Y2
-	 */
+	
 	public int getPosY2() {
 		return posY2;
 	}
 
-	/**
-	 * Permet d'obtenir la seconde position de la barriere sur l'axe des abscisses (X)
-	 * @return la position X2
-	 */
+	
 	public int getPosX2() {
 		return posX2;
 	}
 
-	/**
-	 * Permet de modifier la valeur de la premiere position de la barriere sur l'axe des ordonnees (Y)
-	 * @param posY1 la position Y1 a modifier
-	 */
+	
 	public void setPosY1(int posY1) {
 		this.posY1 = posY1;
 	}
 
-	/**
-	 * Permet de modifier la valeur de la premiere position de la barriere sur l'axe des abscisses (X)
-	 * @param posX1 la position X1 a modifier
-	 */
+	
 	public void setPosX1(int posX1) {
 		this.posX1 = posX1;
 	}
 
-	/**
-	 * Permet de modifier la valeur de la seconde position de la barriere sur l'axe des ordonnees (Y)
-	 * @param posY2 la position Y2 a modifier
-	 */
+	
 	public void setPosY2(int posY2) {
 		this.posY2 = posY2;
 	}
 	
-	/**
-	 * Permet de modifier la valeur de la seconde position de la barriere sur l'axe des abscisses (X)
-	 * @param posX2 la position X2 a modifier
-	 */
+	
 	public void setPosX2(int posX2) {
 		this.posX2 = posX2;
 	}
